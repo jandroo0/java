600e4983f50ed84b6d11319c9fbcc4fe43e4138d
jandroo0 <fortenacer@gmail.com>
2020-08-13 22:23:04 -0400

    add windows msg

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -2,6 +2,7 @@ package app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World 2");
+        System.out.println("Hello World!");
+        System.out.println("Message from windows pc!");
     }
 }
