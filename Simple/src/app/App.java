f095b7b2ee3dfb9e5a0e6c00aa3b6f7857198eb0
jandroo0 <fortenacer@gmail.com>
2020-08-13 22:44:48 -0400

    cleared main method -windows

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -2,11 +2,6 @@ package app;
 
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World!!!");
-=======
-        System.out.println("Hello World!");
-        System.out.println("Message from windows pc!");
->>>>>>> 600e4983f50ed84b6d11319c9fbcc4fe43e4138d
+        // wtf was that , fixed the code. back to blank scren
     }
 }
