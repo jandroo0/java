7c40a160a8eb5b36a93f653062e8adc640464ae0
jandroo0 <fortenacer@gmail.com>
2020-08-14 12:20:56 -0400

    changed directories

--- Simple/src/app/App.java
+++
@@ -1,8 +0,0 @@
-package app;
-
-public class App {
-    public static void main(String[] args) {
-        // wtf was that , fixed the code. back to blank scren
-        System.out.println("Hello World");
-    }
-}
