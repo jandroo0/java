0e0f0da9707267b1708e0544c7af76fedc2391be
jandroo0 <fortenacer@gmail.com>
2020-08-13 21:46:48 -0400

    Created

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -3,5 +3,6 @@ package app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is an edit from my windows pc!");
     }
 }
