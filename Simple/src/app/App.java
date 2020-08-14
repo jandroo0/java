b32a235de50de6a41d196370b07bf36ec04af6ab
jandroo0 <fortenacer@gmail.com>
2020-08-13 22:47:26 -0400

    math problem mac @ school 10:47

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -3,5 +3,10 @@ package app;
 public class App {
     public static void main(String[] args) {
         // wtf was that , fixed the code. back to blank scren
+
+        // at school
+
+        int a = 2 + 1;
+        System.out.println(a);
     }
 }
