ce0ada7a4615766792dacb65916af3ad61c400dd
jandroo0 <fortenacer@gmail.com>
2020-08-13 22:02:04 -0400

    Changed msg -windows

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("This is an edit from my windows pc!");
+        System.out.println("I am learning how to use system control with git!");
     }
 }
