04d13c1271effba0cfd4df01164eeb46301fe8eb
jandroo0 <fortenacer@gmail.com>
2020-08-13 23:09:04 -0400

    Added hello world, removed math problem -windows

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -3,10 +3,6 @@ package app;
 public class App {
     public static void main(String[] args) {
         // wtf was that , fixed the code. back to blank scren
-
-        // at school
-
-        int a = 2 + 1;
-        System.out.println(a);
+        System.out.println("Hello World");
     }
 }
