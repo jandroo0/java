d372072d38e4a2d63ffddc3ea60df72e2d9acada
jandroo0 <fortenacer@gmail.com>
2020-08-13 22:13:10 -0400

    test

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -2,8 +2,6 @@ package app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("This is an edit from my windows pc!");
-        System.out.println("I am learning how to use system control with git!");
+        System.out.println("Hello World 1");
     }
 }
